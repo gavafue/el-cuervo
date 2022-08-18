cb37bcc01859e84f00acbd56c384a190022bb090
Gabriel Vazquez <gavf.vazquez@gmail.com>
2022-08-18 12:27:56 -0300

    Add forgot password page

---
+++ src/Components/Probando/index.js
@@ -0,0 +1,3 @@
+const Probando = () => "hola";
+
+export default Probando;
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "Components/Login"
+import Login from "Components/Login";
+import ForgotPassword from "Components/Login/forgotPassword";
+
 const IndexRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/forgotPassword" element={<ForgotPassword />} />
     </Routes>
   );
 };
